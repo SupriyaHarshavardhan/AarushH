0d5d60eea50bedd642137d90bdade160c3445f5d
Supriya <supriya.harshavardhan@gmail.com>
2021-05-17 22:56:48 +0530

    puch to local repo

---
+++ Simple/src/test/java/projects/Practice1.java
@@ -0,0 +1,12 @@
+package projects;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public void create()
+	{
+		System.out.println("hello");
+	}
+
+}
---
+++ Simple/src/test/java/projects/Practice2.java
@@ -0,0 +1,12 @@
+package projects;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+
+	@Test
+	public void delete()
+	{
+		System.out.println("deleted");
+	}
+}
